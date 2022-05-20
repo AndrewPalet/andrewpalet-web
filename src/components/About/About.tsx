359fd46fcc10e17d2018d2c715232d5dea5aa97c
ectaw6d <Andrew.Palet@McKesson.com>
2022-05-19 20:04:25 -0500

    added contact form + moved spinning cube to home page

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,9 +1,6 @@
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters';
 import './About.scss';
 import { useEffect, useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons';
-import { faMobileAlt } from '@fortawesome/free-solid-svg-icons';
 import Loader from 'react-loaders';
 
 const About = () => {
@@ -31,29 +28,6 @@ const About = () => {
                         Currently I'm looking for a role that will allow me to work on interesting and impactful project. 
                         This position would give me the opportunity to improve my skills and challenge me to grow and perform.</p>
                     <p>When I'm not coding I enjoy playing Rugby, hanging out with friends, playing with my dog, and traveling.</p>
-                </div>
-
-                <div className="stage-cube-cont">
-                    <div className="cubespinner">
-                        <div className="face1">
-                            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-                        </div>
-                        <div className="face2">
-                            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
-                        </div>
-                        <div className="face3">
-                            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-                        </div>
-                        <div className="face4">
-                            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
-                        </div>
-                        <div className="face5">
-                            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-                        </div>
-                        <div className="face6">
-                            <FontAwesomeIcon icon={faMobileAlt} color="#28A4D9" />
-                        </div>
-                    </div>
                 </div>
             </div>
             <Loader type="pacman" active/>
--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,12 +1,14 @@
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters';
 import './Contact.scss';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Loader from 'react-loaders';
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
     const title = "Contact Me"
     const titelArray = Array.from({length: title.length}, (_,i) => title[i])
+    const refForm = useRef()
 
     useEffect(() => {
 		const timer = setTimeout(() => {
@@ -14,6 +16,20 @@ const Contact = () => {
 		}, 3000);
 		return () => clearTimeout(timer);
 	}, []);
+
+    const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        emailjs.sendForm('service_vmkji3x', 'contact_form', e.currentTarget, 'dse7K1sfMFWyR3c9d').then(
+            () => {
+                alert('Message successfully sent!')
+                window.location.reload();
+            },
+            () => {
+                alert('Failed to send the message, please try again!')
+            }
+        )
+    }
 
     return(
         <>
@@ -24,8 +40,29 @@ const Contact = () => {
                         strArray={titelArray}
                         index={15} />
                     </h1>
-                    <p> Hello people</p>
-                    <p>More writing about myself</p>
+                    <p>If you're looking for a trustworthy developer who's skills and values align 
+                        with an open position, feel free to contact me via form below.</p>
+                    <div className="contact-form">
+                        <form ref={refForm.current} onSubmit={sendEmail}>
+                            <ul>
+                                <li className="half">
+                                    <input type="text" name="name" placeholder="Name" required />
+                                </li>
+                                <li className="half">
+                                    <input type="email" name="email" placeholder="Email" required />
+                                </li>
+                                <li>
+                                    <input type="text" name="subject" placeholder="Subject" required />
+                                </li>
+                                <li>
+                                    <textarea name="message" placeholder="Message" required />
+                                </li>
+                                <li>
+                                    <input type="submit" className='flat-button' value="SEND" />
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
                 </div>
             </div>
             <Loader type="pacman" active />
