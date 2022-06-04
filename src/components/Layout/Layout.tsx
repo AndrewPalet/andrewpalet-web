64dd3287bc5b61c21761c4b59f2e5c8847c86eb9
ectaw6d <Andrew.Palet@McKesson.com>
2022-06-04 14:56:22 -0500

    change layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,24 +1,18 @@
-import { Outlet } from 'react-router-dom';
-import Sidebar from '../Sidebar/Sidebar';
-import './Layout.scss';
+import { Outlet } from 'react-router-dom'
+import Sidebar from '../Sidebar/Sidebar'
+import './Layout.scss'
 
 const Layout = () => {
-    return (
-        <div className="App">
-            <Sidebar />
-            <div className="page">
-                <span className="tags top-tags">&lt;body&gt;</span>
-
-                <Outlet />
-
-                <span className="tags bottom-tags">
-                &lt;/body&gt;
-                <br />
-                <span className="bottom-tag-html">&lt;/html&gt;</span>
-                </span>
-            </div>
-        </div>
-    )
+  return (
+    <div className="App">
+      <div className="page">
+        <Sidebar />
+        <span className="tags top-tag">&lt;body&gt;</span>
+        <Outlet />
+        <span className="tags bottom-tag">&lt;/body&gt;</span>
+      </div>
+    </div>
+  )
 }
 
-export default Layout;
+export default Layout
